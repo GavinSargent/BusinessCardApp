22714c8580b71b6e1c5157dc572fa19042367e0e
Gavin Sargent <gavin.sargent3535@gmail.com>
2023-08-16 16:56:37 -0600

    Added image, phone number, and divider to app. Edited color scheme

--- BusinessCardApp/ContentView.swift
+++ BusinessCardApp/ContentView.swift
@@ -13,13 +13,30 @@ struct ContentView: View {
             Color(red: 0.10, green: 0.74, blue: 0.61)
                 .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
             VStack {
-                Image(systemName: "book")
-                    .imageScale(.large)
+                Image("IMG_3121")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
                     .foregroundColor(.accentColor)
+                    .frame(width: 150.0, height: 150.0)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.white))
                 Text("Gavin Sargent")
-                    .font(Font.custom("Bilbo-Regular.ttf", size: 40))
-                    .foregroundColor(Color.black)
+                    .font(Font.custom("Bilbo-Regular", size: 50))
+                    .foregroundColor(Color(uiColor: UIColor(red: 0.95, green: 0.77, blue: 0.06, alpha: 1.00)))
                     .bold()
+                Text("iOS Developer")
+                    .foregroundColor(Color(uiColor: UIColor(red: 0.95, green: 0.77, blue: 0.06, alpha: 1.00)))
+                    .font(.system(size: 25))
+                Divider()
+                ZStack{
+                    Capsule()
+                        .foregroundColor(Color.white)
+                        .frame(height: 40)
+                        .padding()
+                    Text("801-906-3827")
+                        .foregroundColor(Color.black)
+                        
+                }
             }
             .padding()
         }
